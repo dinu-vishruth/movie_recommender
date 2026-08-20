ce4505dc48ec383f011e40d2cc926190f1d84b44
dinu-vishruth <dinuvishruth123@gmail.com>
2026-08-20 16:14:00 +0530

    Set up CI/CD pipeline

--- tests/test_recommender.py
+++ tests/test_recommender.py
@@ -1,2 +1,10 @@
-def test_basic():
-    assert 1 + 1 == 2
+from data_prep import load_movie_lens
+
+
+def test_movie_lens_data_loads():
+    movies, ratings = load_movie_lens()
+
+    assert movies is not None
+    assert ratings is not None
+    assert len(movies) > 0
+    assert len(ratings) > 0
